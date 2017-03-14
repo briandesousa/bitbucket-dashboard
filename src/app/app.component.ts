abc2ed2ccd5b85c592f6452e6ecc8d74f7dab460
Brian De Sousa <Brian De Sousa>
2017-03-13 21:31:56 -0400

    implement simple dashboard UI component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
-}
+  title= 'Branch Banking Dashboard';
+  projectName : string = 'BBSALES';
+  instructions : string = `This dashboard displays a list of Git repositories in the ${this.projectName} project in BitBucket.`;
+
+  repositories : Repository[] = [
+    new Repository("Repo1", 103, 34),
+    new Repository("Repo2", 4, 23),
+    new Repository("Repo3", 59, 12)
+  ];
+};
+
+/**
+ * A source code repository within a BitBucket project.
+ */
+export class Repository { 
+  constructor(
+    public name : string, 
+    public branchCount : number,
+    public openPullRequestCount : number) {}
+};
