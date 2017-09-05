4b12b225b8a658afd47e83e955d78c95f75f6bee
Brian De Sousa <briandesousa1@gmail.com>
2017-09-04 20:14:04 -0400

    remove Angular material dependency

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { MaterialModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { StatisticComponent } from './components/statistic/statistic.component';
@@ -18,8 +17,7 @@ import { RepositoryListComponent } from './components/repository-list/repository
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule,
-    MaterialModule
+    HttpModule
   ],
   providers: [
     RepositoryListService
